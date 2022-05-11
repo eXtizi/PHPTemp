9812012d04bb94dec28d8702f01e7567de4a6cd5
Pramuditha Vidushan <75202685+eXtizi@users.noreply.github.com>
2022-05-11 20:59:37 +0530

    Update fiverr.php

--- web/fiverr.php
+++ web/fiverr.php
@@ -107,15 +107,15 @@ else{
 }
 
 
-/*<?php
-$headers = [
-		"Cache-Control: max-age=0",
-		"Upgrade-Insecure-Requests: 1",
-		"User-Agent: Mozilla/5.0 (Linux; Android 9; vivo 1904) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36",
-		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-		"Accept-Language: si-LK,si;q=0.9,en-US;q=0.8,en;q=0.7",
-	];
-$url = "https://www.fiverr.com/rashawn732/";
+//<?php
+//$headers = [
+//		"Cache-Control: max-age=0",
+//		"Upgrade-Insecure-Requests: 1",
+//		"User-Agent: Mozilla/5.0 (Linux; Android 9; vivo 1904) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36",
+//		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+//		"Accept-Language: si-LK,si;q=0.9,en-US;q=0.8,en;q=0.7",
+//	];
+/*$url = "https://www.fiverr.com/rashawn732/";
 $ch = curl_init();
 curl_setopt_array($ch, [
 	CURLOPT_URL => $url,
